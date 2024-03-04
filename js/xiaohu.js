451434c48e1415a770ede0ffa01e5598c81a3ff2
ql <578148056@qq.com>
2024-03-04 14:12:50 +0000

    update js/xiaohu.js.
    Signed-off-by: ql <578148056@qq.com>

--- js/xiaohu.js
+++ js/xiaohu.js
@@ -1,13 +1,13 @@
 var rule = {
     title: '小胡',
-    host: 'http://bb.xzam.cn',
+    host: 'http://cc.xzam.cn',
     homeUrl: '/api.php/provide/vod?ac=detail',
     searchUrl: '/api.php/provide/vod?ac=detail&wd=**&pg=fypage',
     detailUrl: '/api.php/provide/vod?ac=detail&ids=fyid',
     url: '/api.php/provide/vod?ac=detail&t=fyclass&pg=fypage&f=',
     class_name: '电影&国剧&美剧&韩剧&动漫',
     class_url: '20&1&2&3&4',
-    tab_rename:{'公众号：小胡不胡':'东辰影视'}, 
+    tab_rename:{'公众号：小胡不胡':'奇乐技术网'}, 
     推荐: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
     一级: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
     二级: `js:
